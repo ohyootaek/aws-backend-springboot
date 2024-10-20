ab26707f900f57d7942a9fc650e110cd1654bcfb
ohyootaek <cariros@naver.com>
2024-10-20 20:58:17 +0900

    cors 설정변경

--- src/main/java/com/study/config/CorsConfig.java
+++ src/main/java/com/study/config/CorsConfig.java
@@ -10,7 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")  // 모든 엔드포인트 허용
-        .allowedOrigins("http://www.cariros.store", "https://www.cariros.store")
+        .allowedOrigins("http://cariros.store", "https://cariros.store", "http://www.cariros.store",
+            "https://www.cariros.store")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true);  // 인증 정보도 포함 가능
