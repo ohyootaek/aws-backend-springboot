baeb54bf1668cfa8fda53e9ee5c78f0a09713c4c
ohyootaek <cariros@naver.com>
2024-10-23 01:28:53 +0900

    설정변경

---
+++ src/main/java/com/study/config/CorsConfig.java
@@ -0,0 +1,35 @@
+package com.study.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+  private final Environment environment;
+
+  public CorsConfig(Environment environment) {
+    this.environment = environment;
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    String activeProfile = environment.getActiveProfiles()[0]; // 현재 활성화된 프로필 가져오기
+
+    if ("prod".equals(activeProfile)) {
+      registry.addMapping("/**")
+          .allowedOrigins("http://cariros.store", "https://cariros.store", "http://www.cariros.store", "https://www.cariros.store")
+          .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+          .allowedHeaders("*")
+          .allowCredentials(true);
+    } else if ("local".equals(activeProfile)) {
+      registry.addMapping("/**")
+          .allowedOrigins("http://localhost:3000") // 로컬 환경에 맞는 origin
+          .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+          .allowedHeaders("*")
+          .allowCredentials(true);
+    }
+  }
+}
